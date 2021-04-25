7078cba039daa7680f2e3b3645790220dfd82ae8
gowthamaraj <gowthamaraj360@gmail.com>
2021-04-26 03:48:37 +0530

    test download

--- script.py
+++ script.py
@@ -1,5 +1,20 @@
-from ftplib import FTP
+import ftplib
 
-ftp = FTP('139.59.59.19');
-ftp.login('anonymous','anonymous');
-print(ftp.retrlines('LIST'));
+FTP_SERVER_URL = 'ftp.be.debian.org'
+DOWNLOAD_DIR_PATH = '/pub/linux/kernel/v5.x/'
+DOWNLOAD_FILE_NAME = 'ChangeLog-5.0'
+
+
+def ftp_file_download(server, username):
+    ftp_client = ftplib.FTP(server, username)
+    ftp_client.cwd(DOWNLOAD_DIR_PATH)
+    try:
+        with open(DOWNLOAD_FILE_NAME, 'wb') as file_handler:
+            ftp_cmd = 'RETR %s' %DOWNLOAD_FILE_NAME
+            ftp_client.retrbinary(ftp_cmd,file_handler.write)
+            ftp_client.quit()
+    except Exception as exception:
+        print('File could not be downloaded:',exception)
+
+if __name__ == '__main__':
+    ftp_file_download(server=FTP_SERVER_URL,username='anonymous')
